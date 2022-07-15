c11676cd2d0999338501c68a4088f1972d2b6d2a
dac-uma <felipe@uma.es>
2022-07-15 07:11:29 +0200

    corregido path mol2

--- index.php
+++ index.php
@@ -24,70 +24,57 @@ $host= gethostname();
 //echo $_SERVER['LOCAL_ADDR'].$host;
 if(!isset($_GET['mol'])){echo "Use http://molserver.feliperomero.es?mol=DB00114&num=6";exit;}
 $molecule=$_GET['mol'];
-if(isset($_GET['num']))$num=$_GET['num'];else$num=4;
+if(isset($_GET['num']))$num=$_GET['num'];else$num=8;
 //if(isset($_GET['debug']))$debug=true;
+
+
 //*****************************************************************************************************************
 //               Si no existe el archivo mol2, lo descargamos de la base de datos y lo convertimos con openbabel
 //*****************************************************************************************************************
-
-
-if(!file_exists("mol2/$molecule.mol2")){
-	//$debug=true;
-	$path="https://go.drugbank.com/structures/small_molecule_drugs/{$molecule}.mol";
-	if($debug)
-	echo "downloading $path<br>";
-	if(!($src = file_get_contents($path)) ){echo "negativo";exit;}
-	if($host=="cactus")
-	$archivo="mol/{$molecule}.mol";
-	else
-	$archivo="/var/molecules/mol/{$molecule}.mol";
-	
-	file_put_contents($archivo, $src);
-
-
 if($host=="cactus")
-{
-	$convert="\"C:\\Program Files\\OpenBabel-2.4.1\\obabel\" --title {$molecule} -i mol d:\\datos\\mol2imageserver\\mol\\{$molecule}.mol -o ml2 -O d:\\datos\\mol2imageserver\\mol2\\{$molecule}.mol2";
-	//$convert="\"C:\\Program Files\\OpenBabel-2.4.1\\obabel\" -H";
-}
-else
-{
-	$convert="obabel --title {$molecule} -i mol /var/molecules/mol/{$molecule}.mol -o ml2 -O /var/molecules/mol2/{$molecule}.mol2";
-
-}
-	if($debug)echo "<br>$convert<br>";
-	$salida=shell_exec(($convert));
-	//echo $salida;
-	$lineas=preg_split('/\r\n|\r|\n/', $salida);
-	for($i=0;$i<count($lineas)-1;$i++)
 	{
-		if($debug)echo "<a href=$lineas[$i] target=_blank >$lineas[$i]</a><br>";
-		else echo "$lineas[$i]<br>";
+	$mol2file="mol2/$molecule.mol2";
+	$molfile="mol/{$molecule}.mol";
+	$codeexec="d:/onedrive/proyectos/cudaMol/cmake-build-debug-visual-studio/Debug/mol2image.exe $mol2file $num";
+	$convert="\"C:\\Program Files\\OpenBabel-2.4.1\\obabel\" --title {$molecule} -i mol d:\\datos\\mol2imageserver\\mol\\{$molecule}.mol -o ml2 -O d:\\datos\\mol2imageserver\\mol2\\{$molecule}.mol2";
+	}
+else
+	{
+	$mol2file="/var/molecules/mol2/$molecule.mol2";
+	$molfile="/var/molecules/mol/{$molecule}.mol";
+	$codeexec="/opt/mol2image/mol2image $mol2file $num";
+	$convert="obabel --title {$molecule} -i mol /var/molecules/mol/{$molecule}.mol -o ml2 -O /var/molecules/mol2/{$molecule}.mol2";
 	}
 
-}
 
-if($host=="cactus")
-{
-$molecula="mol2/$molecule.mol2";
-$codeexec="d:/onedrive/proyectos/cudaMol/cmake-build-debug-visual-studio/Debug/mol2image.exe $molecula $num";
-}
-else
-{
-	$molecula="/var/molecules/mol2/$molecule.mol2";
-	$codeexec="/opt/mol2image/mol2image $molecula $num";
-	
-}
+if(!file_exists($mol2file)){
+		$path="https://go.drugbank.com/structures/small_molecule_drugs/{$molecule}.mol";
+		if($debug)	echo "downloading $path<br>";
+		if(!($src = file_get_contents($path)) ){echo "negativo";exit;}
+		
+		file_put_contents($molfile, $src);
+
+		if($debug)echo "<br>$convert<br>";
+		$salida=shell_exec(($convert));
+		$lineas=preg_split('/\r\n|\r|\n/', $salida);
+		for($i=0;$i<count($lineas)-1;$i++)
+		{
+			if($debug)echo "<a href=$lineas[$i] target=_blank >$lineas[$i]</a><br>";
+			else echo "$lineas[$i]<br>";
+		}
+
+		}
+
 //*****************************************************************************************************************
 //               Obtiene N vistas
 //*****************************************************************************************************************
-
 
 if($debug)echo "<br>Executing: $codeexec<br><br>";
 $time_start = microtime(true); 
 $archivos=shell_exec($codeexec);
 $lineas=preg_split('/\r\n|\r|\n/', $archivos);
 $time_end = microtime(true); 
+
 //*****************************************************************************************************************
 //               Mostramos los archivos generados (salida del shell_exec)
 //*****************************************************************************************************************
